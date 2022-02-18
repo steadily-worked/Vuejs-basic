1bd709e1d911323318b9af41b148d860cf23502d
steadily-worked <iamsangminpark@gmail.com>
2022-02-18 12:06:00 +0900

    add: methods

--- demo2/app.js
+++ demo2/app.js
@@ -6,6 +6,16 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org",
     };
   },
+  methods: {
+    outputGoal: function () {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
